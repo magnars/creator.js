cfee11d92a326f6329287874032aed4b4d6925c4
Magnar Sveen <magnars@gmail.com>
2012-06-13 07:53:32 +0200

    Basic create.

--- test/creator-test.js
+++ test/creator-test.js
@@ -7,8 +7,17 @@ if (typeof require === "function" && typeof module !== "undefined") {
   "use strict";
 
   buster.testCase('Creator', {
-    "does stuff": function () {
-      assert(true);
+    "creates a function that creates objects": function () {
+      var banana = {
+        create: creator("banana", []),
+        color: "green"
+      };
+
+      var obj = banana.create();
+      assert.equals(obj.color, "green");
+
+      obj.color = "brown";
+      assert.equals(banana.color, "green");
     }
   });
 }());
