0189bb332ab4f08c6325065136c37231de7fdf56
Magnar Sveen <magnars@gmail.com>
2012-06-13 22:51:18 +0200

    Strict.

--- test/creator-test.js
+++ test/creator-test.js
@@ -95,6 +95,31 @@ if (typeof require === "function" && typeof module !== "undefined") {
         var b = this.banana.create({ color: "yellow" });
         assert.equals(b.color, "yellow");
       }
+    },
+
+    "when strict": {
+      setUp: function () {
+        this.banana = {
+          create: creator("banana", {
+            required: ["color"],
+            defaults: { curvature: 23 },
+            strict: true
+          })
+        };
+      },
+
+      "complains about unknown parameters": function () {
+        try {
+          this.banana.create({
+            color: "brown",
+            curvature: 42,
+            musical: "Which Witch"
+          });
+        } catch (e) {
+          var expected = "banana.create: unknown params { musical }";
+          assert.equals(e.message, expected);
+        }
+      }
     }
 
   });
